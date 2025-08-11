aea7a43af2bc50e510250ac57cb93135c902c6b4
Tobias Wittwer <55980052+tfwittwer@users.noreply.github.com>
2025-08-11 18:32:34 +0200

    Update MyLidarPointCloudActor.cpp

--- MyLidarPointCloudActor.cpp
+++ MyLidarPointCloudActor.cpp
@@ -1,4 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/*    Copyright (C) 2025 Tobias Wittwer
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/. */
+	
 #include "MyLidarPointCloudActor.h"
 #include "CoreGlobals.h"
 #include "HAL/FileManagerGeneric.h"
@@ -117,3 +121,4 @@ void AMyLidarPointCloudActor::LoadClouds(TCHAR* folder)
 
 	UE_LOG(LogTemp, Display, TEXT("Point cloud offset: %s"), *FString(offset.ToString()));
 }
+
